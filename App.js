a05e6e782310419c1805eb5481eedda35e6495ef
dwyertyrell <Dwyertyrell@gmail.com>
2025-07-15 09:17:14 +0100

    feat: Add ShoppingLists and Welcome components with Firebase integration
    - Implement ShoppingLists component for managing shopping lists with Firestore.
    - Add functionality to fetch, add, and display shopping lists.
    - Create Welcome component for user authentication using Firebase.
    - Integrate anonymous sign-in and navigation to ShoppingLists upon successful sign-in.
    - Update package.json to include necessary dependencies for navigation and Firebase.

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { getAuth } from "firebase/auth";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import { initializeAuth, getReactNativePersistence } from "firebase/auth"; // <-- CHANGE HERE!
+
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'; // <-- NEW IMPORT!
+
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+import ShoppingLists from './components/ShoppingLists.js'
+import Welcome from './components/Welcome.js';
+
+const App = () => {
+  const firebaseConfig = {
+  apiKey: "AIzaSyBA1vWZKnkddD5n1N88_mta_Rf4gPiDmFI",
+  authDomain: "chat-app-d1777.firebaseapp.com",
+  projectId: "chat-app-d1777",
+  storageBucket: "chat-app-d1777.firebasestorage.app",
+  messagingSenderId: "915890104856",
+  appId: "1:915890104856:web:2d29a82b446e4ae830b16d",
+  measurementId: "G-7FW2XLWSZ8"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+  
+    // Initialize Firebase Auth with React Native persistence!
+  const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  });
+  
+
+ return (
+    <NavigationContainer>
+      <Stack.Navigator 
+      initialRouteName = 'Welcome'
+      >
+        <Stack.Screen
+          name="Welcome">
+          {props => <Welcome auth={auth} />}
+        </Stack.Screen>
+
+
+        <Stack.Screen
+          name="ShoppingLists">
+          {props => <ShoppingLists db={db} {...props} />}
+       </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
